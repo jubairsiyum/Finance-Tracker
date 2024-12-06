08c5895b29abcfc00b3a4e5196a34acb96356aa7
sHakibMusfiqur <musfiqur.shakib@gmail.com>
2024-12-06 15:52:46 +0600

    up

--- classes/currency_converter.py
+++ classes/currency_converter.py
@@ -1,1 +1,10 @@
 # Shakib's Code
+
+class CurrencyConverter:
+    def __init__(self, rates):
+        self.rates = rates  # rates is a dictionary with currency codes and conversion rates
+
+    def convert(self, amount, from_currency, to_currency):
+        if from_currency == to_currency:
+            return amount
+        return amount / self.rates[from_currency] * self.rates[to_currency]
